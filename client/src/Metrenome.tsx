2a3700283786bd0913185226397224ac41836b29
Hayden Schultz <haydenschultz42@gmail.com>
2019-08-19 10:26:29 -0700

    converting metrenome to typescript

--- client/src/Metrenome.tsx
+++ client/src/Metrenome.tsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react'
 // Webpack will load the audio files
 import click1 from './click1.wav';
 import click2 from './click2.wav';
+import { number } from 'prop-types';
 
 
 const Metronome: React.FC = () => {
@@ -10,7 +11,7 @@ const Metronome: React.FC = () => {
       const [bpm, setBpm] = useState([]);
       const [playing, setPlaying] = useState<boolean | null>(null);
       const [count, setCount] = useState(4)
-    };
+    
 
     // Create Audio objects with the files Webpack loaded,
     // and we'll play them later.
@@ -18,65 +19,67 @@ const Metronome: React.FC = () => {
     const click2 = new Audio(click2);
   
 
-  playClick = () => {
-    const { count, bpm } = useState();
+  function playClick() {
+    const [ count, bpm ] = useState();
 
     // The first beat will have a different sound than the others
-    if(count % bpm === 0) {
+    if(Number(count) % Number(bpm) === 0) {
       click2.play();
     } else {
       click1.play();
     }
 
     // Keep track of which beat we're on
-    useState(state => ({
-      count: (state.count + 1) % state.bpm
-    }));
-  }
+    useState(){(
+      count: (Number(setCount) + 1) % Number(setBpm)
 
-  startStop = () => {
-    if(useState.playing) {
-      // Stop the timer
-      clearInterval(timer);
-      useState({
-        playing: false
-      });
-    } else {
-      // Start a timer with the current BPM
-      timer = setInterval(playClick, (60 / state.bpm) * 1000);
-      useState({
-        count: 0,
-        playing: true
-        // Play a click "immediately" (auseState finishes)
-      }, playClick);
     }
-  }
-
-  handleBpmChange = event => {
-    const bpm = event.target.value;
-
-    if(this.state.playing) {
-      // Stop the old timer and start a new one
-      clearInterval(this.timer);
-      this.timer = setInterval(this.playClick, (60 / bpm) * 1000);
-
-      // Set the new BPM, and reset the beat counter
-      useState({
-        count: 0,
-        bpm
-      });
-    } else {
-      // Otherwise just update the BPM
-      useState({ bpm });
+    } 
+    
+    function startStop() {
+        if(playing) {
+            // Stop the timer
+            clearInterval(timer);
+            useState({
+                playing: false
+            });
+        } else {
+            // Start a timer with the current BPM
+            timer = setInterval(playClick, (60 / state.bpm) * 1000);
+            useState({
+                count: 0,
+                playing: true
+                // Play a click "immediately" (auseState finishes)
+            }, playClick);
+        }
     }
-
-  }
-
-  render() {
-    const { playing, bpm } = this.state;
+    
+    function handleBpmChange(e: React.MouseEvent):void {
+        e.preventDefault()
+        const bpm = event.target.value;
+        
+        if(playing) {
+            // Stop the old timer and start a new one
+            clearInterval(timer);
+            timer = setInterval(playClick, (60 / bpm) * 1000);
+            
+            // Set the new BPM, and reset the beat counter
+            useState({
+                count: 0,
+                bpm
+            });
+        } else {
+            // Otherwise just update the BPM
+            useState({ bpm });
+        }
+        
+    }
+    
+    
+    // const [playing, bpm ] = setState();
 
     return (
-      <div className="metronome">
+        <div className="metronome">
         <div className="bpm-slider">
           <div>{bpm} BPM</div>
           <input
@@ -84,15 +87,18 @@ const Metronome: React.FC = () => {
             min="60"
             max="240"
             value={bpm}
-            onChange={this.handleBpmChange} />
+            onChange={handleBpmChange} />
         </div>
-        <button onClick={this.startStop}>
+        <button onClick={startStop}>
           {playing ? 'Stop' : 'Start'}
         </button>
       </div>
     );
-  }
 }
+
+
+
+
 
 export default Metronome;
 
