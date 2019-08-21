f0f7f2197552ba9865f439dbecdccbf53ee80902
Hayden Schultz <haydenschultz42@gmail.com>
2019-08-20 17:12:32 -0700

    delete route works, need show

--- client/src/favoriteList.tsx
+++ client/src/favoriteList.tsx
@@ -2,7 +2,13 @@ import React, {useState, useEffect} from 'react';
 import './App.css';
 import axios from 'axios';
 
-
+export interface ISong {
+  _id: string;
+  song_title: string;
+  artist: string;
+  tempo: number;
+  
+}
 
 
 export interface IFavorite{
@@ -14,6 +20,7 @@ export interface IFavorite{
 
   const FavoriteList: React.FC = () => {
     const [favorites, setFavorite] = useState<IFavorite[]>([])
+    const [songs, setSong] = useState<ISong[]>([])
 
     // set isbn search parameters
  
@@ -23,13 +30,23 @@ export interface IFavorite{
           setFavorite(res.data)
         })
       })
-    
+
+    function deleteSong(favorites: any){
+      axios.delete(`/song/${favorites._id}`, {
+        }).then( () => {
+            axios.get(`/song`).then( (response) =>{
+                setFavorite(response.data)
+                console.log('deleted')
+            })
+        })
+    }
       
     
       var favoriteData = favorites.map((favorite, id) => {
         return(
          <div>
           <p>Song Title: {favorite.song_title} Tempo: {favorite.tempo}</p>
+          <button className="delete" onClick={()=> deleteSong(favorite)}>Delete Song</button>
           </div>
       )})
      
